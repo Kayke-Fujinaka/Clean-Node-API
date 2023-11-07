ccd9630cedd474f4d0365ce51da2e06028ad23de
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-07 09:48:24 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.export = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
