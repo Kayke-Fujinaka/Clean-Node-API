9e6daa7875209b1447d28adaa3c7dc5bc9f274c6
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-12-22 08:33:20 -0300

    chore: add coverage provider to instrument code for coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.export = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
