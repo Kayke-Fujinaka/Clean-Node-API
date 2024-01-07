1691b224be1f30ee33635e1d1aa6163de77a93df
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2024-01-07 11:14:12 -0300

    fix: change uri mongodb connect

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,7 +4,7 @@ export const MongoHelper = {
   client: (null as unknown) as MongoClient,
 
   async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(uri)
+    this.client = await MongoClient.connect(process.env.MONGO_URL)
   },
 
   async disconnect(): Promise<void> {
