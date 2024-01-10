5c2ac05b32d0cf9e16bd21839efa3adb5036e7ff
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2024-01-10 19:16:43 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -11,9 +11,13 @@ export const MongoHelper = {
 
   async disconnect(): Promise<void> {
     await this.client.close()
+    this.client = null
   },
 
-  getCollection(name: string): Collection {
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   },
 
