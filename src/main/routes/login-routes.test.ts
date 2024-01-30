0f62096175824637a70d7a8dd8f3d085f9a6e27f
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2024-01-30 07:36:28 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,11 @@
+import { hash } from 'bcrypt'
+import { type Collection } from 'mongodb'
 import request from 'supertest'
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async() => {
@@ -14,7 +18,7 @@ describe('Login Routes', () => {
 
   beforeEach(async() => {
     jest.useRealTimers()
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -27,6 +31,24 @@ describe('Login Routes', () => {
           email: 'Kayke@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async() => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Kayke',
+        email: 'Kayke@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'Kayke@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
