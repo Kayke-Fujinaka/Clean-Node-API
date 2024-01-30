d41f9e2d7d134a5be22ab851acb6cd07c5071b2f
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2024-01-30 07:32:08 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'ha&1=1-1r3_a'
 }
